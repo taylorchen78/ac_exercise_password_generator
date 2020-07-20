e5b81c6de4874a59521f272a67a97b8b259af565
Taylor Chen <s68175@gmail.com>
2020-07-20 19:19:01 +0800

    Init password generator project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+// template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express listening on port:${port}`)
+})
